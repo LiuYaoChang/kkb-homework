645db13e2e43a9e22ebdc0ca13cbbddd357fb2dd
liuyaochang <tennislover_l@163.com>
2019-12-06 09:42:52 +0800

    删除远端dist

--- dist/common/ValidateDecorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Validate(options) {
-    return function (target, key, Desc) {
-    };
-}
-exports.Validate = Validate;
-//# sourceMappingURL=ValidateDecorator.js.map
